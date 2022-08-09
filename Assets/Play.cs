1da8ef12b9a5ecf971a15d8cb7fc12ea57ab65c5
Jason Ortiz <8240549+jason-ortiz@users.noreply.github.com>
2022-08-09 14:13:45 -0400

    Add Reverse Animation interaction

--- Assets/Play.cs
+++ Assets/Play.cs
@@ -7,6 +7,8 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Play : MonoBehaviour
 {
     private PlayableDirector m_playableDirector;
+    private bool m_isReverse = false;
+    private bool m_isPaused = false;
 
     void Start()
     {
@@ -15,49 +17,64 @@ public class Play : MonoBehaviour
 
     void Update()
     {
-    }
-
-    public void OnSelectEnter(SelectEnterEventArgs args)
-    {
-        if (args.interactor.CompareTag("RightController"))
+        if (m_isReverse && !m_isPaused)
         {
-            switch (m_playableDirector.state)
+            double currentTime = m_playableDirector.time - Time.deltaTime;
+            if (currentTime < 0)
             {
-                case PlayState.Playing: 
-                    m_playableDirector.Pause();
-                    return;
-                case PlayState.Paused:
-                    m_playableDirector.Resume();
-                    return;
+                currentTime = 0;
+            }
+
+            m_playableDirector.time = currentTime;
+            m_playableDirector.Evaluate();
+
+            if (currentTime == 0)
+            {
+                // Loop around
+                m_playableDirector.time = m_playableDirector.playableAsset.duration - 0.01;
             }
         }
     }
 
-    /*[RequireComponent(typeof(PlayableDirector))]
-public class ReversePlayback : MonoBehaviour {
-    private PlayableDirector director;
- 
-    // Use this for initialization
-    void Start() {
-        director = GetComponent<PlayableDirector>();
-        director.Stop();
-        director.time = director.playableAsset.duration - 0.01;
-        director.Evaluate();
-    }
- 
-    // Update is called once per frame
-    void Update() {
-        double t = director.time - Time.deltaTime;
-        if (t < 0)
-            t = 0;
- 
-        director.time = t;
-        director.Evaluate();
- 
-        if (t == 0) {
-            director.Stop();
-            enabled = false;
+    public void OnSelectEnter(SelectEnterEventArgs args)
+    {
+        // Play/Pause
+        if (args.interactor.CompareTag("RightController"))
+        {
+            if (!m_isReverse)
+            {
+                switch (m_playableDirector.state)
+                {
+                    case PlayState.Playing: 
+                        m_playableDirector.Pause();
+                        return;
+                    case PlayState.Paused:
+                        m_playableDirector.Resume();
+                        return;
+                }
+            }
+            else
+            {
+                m_isPaused = !m_isPaused;
+                if (m_isPaused)
+                {
+                    m_playableDirector.Pause();
+                }
+            }
+        }
+        // Reverse/Un-reverse
+        else if (args.interactor.CompareTag("LeftController"))
+        {
+            m_isReverse = !m_isReverse;
+            m_isPaused = false; // always unpause when reversing
+            if (m_isReverse)
+            {
+                m_playableDirector.Pause(); // pause PlayableDirector to allow for manual playback evaulation
+            }
+            else
+            {
+                m_playableDirector.Resume(); // give control back to PlayableDirector
+            }
         }
     }
-} */
 }
